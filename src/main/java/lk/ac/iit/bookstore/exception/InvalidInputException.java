3536077bc20b64eee0f866f689c7455e7f4f57c1
Dinith <dinithshenuka@gmail.com>
2025-04-16 00:05:43 +0530

    code documentation update

---
+++ src/main/java/lk/ac/iit/bookstore/exception/InvalidInputException.java
@@ -0,0 +1,9 @@
+package lk.ac.iit.bookstore.exception;
+
+// Exception thrown when input data is invalid
+public class InvalidInputException extends RuntimeException {
+    
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/lk/ac/iit/bookstore/exception/InvalidInputExceptionMapper.java
@@ -0,0 +1,21 @@
+package lk.ac.iit.bookstore.exception;
+
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+// Maps InvalidInputException to HTTP 400 response
+@Provider
+public class InvalidInputExceptionMapper implements ExceptionMapper<InvalidInputException> {
+    
+    @Override
+    public Response toResponse(InvalidInputException exception) {
+        ErrorResponse errorResponse = new ErrorResponse("Invalid Input", exception.getMessage());
+        
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(errorResponse)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
