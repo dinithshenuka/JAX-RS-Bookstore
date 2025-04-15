60c8a29093ad395becffbb663d28c0dc9b6129ea
Dinith <dinithshenuka@gmail.com>
2025-04-15 20:58:58 +0530

    book model added

---
+++ src/main/java/lk/ac/iit/bookstore/model/Book.java
@@ -0,0 +1,83 @@
+package lk.ac.iit.bookstore.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private Long authorId;
+    private String isbn;
+    private int publicationYear;
+    private double price;
+    private int stock;
+
+    // for JSON binding
+    public Book() {
+    }
+
+    // all fields Constructor 
+    public Book(Long id, String title, Long authorId, String isbn, int publicationYear, double price, int stock) {
+        this.id = id;
+        this.title = title;
+        this.authorId = authorId;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
