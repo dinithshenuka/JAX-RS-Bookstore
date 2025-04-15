1a253bfb0874a3497ec1c13afb98bdcbda16c290
Dinith <dinithshenuka@gmail.com>
2025-04-16 00:08:11 +0530

    runner

---
+++ run.sh
@@ -0,0 +1,25 @@
+# Easy Run Script for JAX-RS-Bookstore
+# run - ./run.sh
+# stop - /Applications/apache-tomcat-11.0.6/bin/shutdown.sh
+
+# Tomcat installation directory 
+TOMCAT_HOME=/Users/iammrdp/apache-tomcat-11.0.6
+
+# Build the project 
+mvn clean package
+
+# Copy the WAR file to Tomcat's webapps directory
+cp target/JAX-RS-Bookstore.war $TOMCAT_HOME/webapps/bookstore.war
+
+# Start Tomcat (if not already running)
+$TOMCAT_HOME/bin/startup.sh
+
+echo ""
+echo "Application deployed!"
+echo "Your application should be available at:"
+echo "http://localhost:8080/bookstore/"
+echo "REST API endpoints available at:"
+echo "http://localhost:8080/bookstore/api/"
+echo ""
+echo "Use the following command to stop Tomcat when finished:"
+echo "$TOMCAT_HOME/bin/shutdown.sh"
